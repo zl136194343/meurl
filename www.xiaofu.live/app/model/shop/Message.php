47a47aea5141be6d357a989479bdb0900800cdb4
zhangliang <zl931010>
2023-04-12 13:28:36 +0800

    Changes

--- www.xiaofu.live/app/model/shop/Message.php
+++ www.xiaofu.live/app/model/shop/Message.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace app\model\shop;
-
-class Message
+use app\model\BaseModel;
+class Message extends BaseModel
 {
-
+    public function getMessageInfo($condition, $field)
+    {
+        $res = model('shop')->getInfo($condition, $field);
+        return $this->success($res);
+    }
+    public function pageMessageList($condition, $field,$page,$page_size)
+    {
+        $res = model('shop_message')->pageList($condition, $field,'id desc',$page,$page_size);
+        return $this->success($res);
+    }
+    public function addMessage($data)
+    {
+        $res = model('shop_message')->add($data);
+        return $this->success($res);
+    }
 }
